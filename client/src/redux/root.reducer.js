a03cc94281f49a1ae615489f7aa847bc8715600f
Vikash <52384251+gitsforvikki@users.noreply.github.com>
2023-08-29 18:46:25 +0530

    Update root.reducer.js

--- client/src/redux/root.reducer.js
+++ client/src/redux/root.reducer.js
@@ -1,7 +1,15 @@
 import { combineReducers } from "redux";
 import * as userReducer from './user/user.reducer';
 import * as taskReducer from "./task/task.reducer"
+import * as alertReducer from './alert/alert.reducer';
 export const rootReducer = combineReducers({
     [userReducer.userFeatresKey]  : userReducer.reducer,
-    [taskReducer.taskFeatresKey] : taskReducer.reducer
+    [taskReducer.taskFeatresKey] : taskReducer.reducer,
+    [alertReducer.alertFeatureskey]  :alertReducer.reducer
 });
+
+
+
+
+
+
