f106caf3eeec3bd0194b280f2156ef86270242d5
Vikash <52384251+gitsforvikki@users.noreply.github.com>
2023-08-29 16:40:30 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -45,11 +45,11 @@ app.use('/api/tasks' , require('./router/taskRouter'));
 // });
 
 //to serve the frontend
-app.use(express.static(path.join(__dirname, "././client/build")));
+app.use(express.static(path.join(__dirname, "../client/build")));
 
 app.get("*", function (req, res) {
   res.sendFile(
-    path.join(__dirname, "././client/build/index.html"),
+    path.join(__dirname, "../client/build/index.html"),
     function (err) {
       res.status(500).send(err);
     }
