5a1fc3d0f189d0a80d54878561eb48e5354c1bbf
Vikash <52384251+gitsforvikki@users.noreply.github.com>
2023-08-29 16:18:31 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const dotEnv =  require('dotenv');
 const mongoose = require('mongoose');
-
+const path = require('path');
 
 //initialize express
 const app = express();
